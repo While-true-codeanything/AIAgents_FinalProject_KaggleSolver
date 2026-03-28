bf18a2df70af7c46f71a388aaa6a0b677830896e
While-true-codeanything <pavlov.erg@gmail.com>
2026-03-28 15:41:27 +0300

    test module

---
+++ test_modules_exec.py
@@ -0,0 +1,8 @@
+from src.tools.llm_api_connector import ask_model_response
+from test_components import test_modelapi, test_datatools, test_explorer, test_engineer, test_code_executor
+
+#test_modelapi()
+#test_datatools()
+#test_explorer()
+#test_engineer()
+test_code_executor()
